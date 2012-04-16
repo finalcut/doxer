8f813e1f1ef7a73f1fa3cd94e702e74be92a5108
Bill Rawlinson <bill@rawlinson.us>
2012-04-16 08:49:46 -0400

    begin refactoring to make things more modular.

--- src/routes/section/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-	F3::route('GET /@libraryName/@projectName/newsection', 'doxer\controllers\section\SectionController->form');
-	F3::route('GET /@libraryName/@projectName/@uuid/edit', 'doxer\controllers\section\SectionController->form');
-	F3::route('GET /@libraryName/@projectName/@uuid/save', 'doxer\controllers\section\SectionController->save');
-	F3::route('GET /@libraryName/@projectName//save', 'doxer\controllers\section\SectionController->save');
-?>
